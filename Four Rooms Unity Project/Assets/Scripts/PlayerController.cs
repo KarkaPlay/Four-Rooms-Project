6616ac6877f9479dcad101439715dccd3b978fea
KarpushkinS <c1e2r3g@gmail.com>
2021-09-17 00:54:10 +0300

    Базовая сцена, префаб и скрипт

--- Four Rooms Unity Project/Assets/Scripts/PlayerController.cs
+++ Four Rooms Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    private float speed = 5.0f;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
