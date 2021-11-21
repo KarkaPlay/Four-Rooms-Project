1d6aa1561e80500a459ea1e8e4067402862f7983
Egor147 <egor8313@yandex.ru>
2021-11-22 02:22:41 +0300

    вв

--- Four Rooms Unity Project/Assets/Scripts/Door.cs
+++ Four Rooms Unity Project/Assets/Scripts/Door.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     private float rotationSpeed = 60.0f;
-    public Vector3 close, open;
+    public Vector3  open, close;
     public float openAngle = 120;
     public bool isOpen;
     private Quaternion openQ, closeQ; 
@@ -19,38 +19,24 @@ public class Door : MonoBehaviour
         if(On_The_Minus)
             openAngle *=-1;
 
-        if (open.y == 1 || close.y == 1){
+        close = transform.rotation.eulerAngles;
+
+        if (open.y == 1){
+
             parentRotation = transform.parent.rotation.eulerAngles.y;
-            open.y = 180 + openAngle + parentRotation;
-            close.y = 180 + parentRotation;
 
-            open.x = transform.rotation.eulerAngles.x;  //Это на случай, если начальное х или z равно не нулю. Чтобы оно так и оставалось начальным
-            open.z = transform.rotation.eulerAngles.z;
-
-            close.x = transform.rotation.eulerAngles.x; //смотри коммент на 27
-            close.z = transform.rotation.eulerAngles.z;
+            open = transform.rotation.eulerAngles + new Vector3(0,  openAngle, 0);
+            close = transform.rotation.eulerAngles;
         } else{
-            if (open.z == 1 || close.z == 1){
+            if (open.z == 1){
             parentRotation = transform.parent.rotation.eulerAngles.z;
-            close.z = transform.rotation.eulerAngles.z;
-            open.z = openAngle + parentRotation;
 
-            open.x = transform.rotation.eulerAngles.x;  //смотри коммент на 27 (Принцип тот же. Только координаты другие)
-            open.y = transform.rotation.eulerAngles.y;  
-
-            close.x = transform.rotation.eulerAngles.x; //смотри коммент на 27 (Принцип тот же. Только координаты другие)
-            close.y = transform.rotation.eulerAngles.y; 
+            open = transform.rotation.eulerAngles + new Vector3(0, 0, openAngle);
             } else {
-                if (open.x == 1 || close.x == 1){
+                if (open.x == 1){
                     parentRotation = transform.parent.rotation.eulerAngles.x;
-                    close.x = transform.rotation.eulerAngles.x;
-                    open.x = openAngle + parentRotation;
-
-                    open.y = transform.rotation.eulerAngles.y;  //смотри коммент на 27 (Принцип тот же. Только координаты другие)
-                    open.z = transform.rotation.eulerAngles.z; 
-
-                    close.y = transform.rotation.eulerAngles.y; //смотри коммент на 27 (Принцип тот же. Только координаты другие)
-                    close.z = transform.rotation.eulerAngles.z; 
+                    
+                    open = transform.rotation.eulerAngles + new Vector3(openAngle,0, 0);
             }
             }
         }
