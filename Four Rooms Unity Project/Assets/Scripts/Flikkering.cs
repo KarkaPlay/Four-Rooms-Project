71536d35f74a128bcea6e050040537fcbbd2bc1d
KarkaPlay <c1e2r3g@gmail.com>
2021-12-23 02:57:59 +0300

    Добавлен коридор, скрипты, материалы и модели

---
+++ Four Rooms Unity Project/Assets/Scripts/Flikkering.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Flikkering : MonoBehaviour
+{
+    public bool isFlikkering = false;
+    public float timeDelay;
+    
+    private void Update()
+    {
+        if (!isFlikkering)
+        {
+            StartCoroutine(FlikkeringLight());
+        }
+    }
+
+    IEnumerator FlikkeringLight()
+    {
+        isFlikkering = true;
+
+        //gameObject.GetComponent<Light>().enabled = false;
+        gameObject.GetComponent<Light>().intensity /= 2;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        
+        //gameObject.GetComponent<Light>().enabled = true;
+        gameObject.GetComponent<Light>().intensity *= 2;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+
+        isFlikkering = false;
+    }
+}
